73a6bb85293b29201af818e2a045eadfb2264ddd
刘君 <liujun2@100tal.com>
2017-10-27 15:15:52 +0800

    composer 安装

--- composer.sh
+++ composer.sh
@@ -2,10 +2,14 @@
 
 #composer安装
 
-cd /usr/local/sbin
-php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
-php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
-php composer-setup.php
-php -r "unlink('composer-setup.php');"
+#cd /usr/local/sbin
+#php -r "copy('https://getcomposer.org/installer', 'composer-setup.php');"
+#php -r "if (hash_file('SHA384', 'composer-setup.php') === '669656bab3166a7aff8a7506b8cb2d1c292f042046c5a994c43155c0be6190fa0355160742ab2e1c88d40d5be660b410') { echo 'Installer verified'; } else { echo 'Installer corrupt'; unlink('composer-setup.php'); } echo PHP_EOL;"
+#php composer-setup.php
+#php -r "unlink('composer-setup.php');"
+#
 
+
+
+curl -sS https://getcomposer.org/installer | php -- --install-dir=/usr/local/sbin
 ln -s /usr/local/sbin/composer.phar /usr/local/sbin/composer
